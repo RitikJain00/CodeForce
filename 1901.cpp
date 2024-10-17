70a17b3f067f5f222d129bbb0335062a3861a76e
Ritik <ritikjain590@gmail.com>
2024-10-18 00:31:34 +0530

    Coprime

---
+++ 1901.cpp
@@ -0,0 +1,121 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define fr(i, n) for (int i = 0; i < (n); i++)
+#define rep(i, a, n) for (int i = (a); i <= (n); i++)
+#define nl cout << "\n"
+#define dbg(var) cout << #var << "=" << var << " "
+#define all(v) v.begin(), v.end()
+#define sz(v) (int)(v.size())
+#define srt(v) sort(v.begin(), v.end())         // sort
+#define mxe(v) *max_element(v.begin(), v.end()) // find max element in vector
+#define mne(v) *min_element(v.begin(), v.end()) // find min element in vector
+#define unq(v) v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed
+#define bin(x, y) bitset<y>(x)
+using namespace std;
+int MOD = 1e9 + 7; // Hardcoded, directly change from here for functions!
+
+void modadd(int &a, int b) { a = ((a % MOD) + (b % MOD)) % MOD; }
+void modsub(int &a, int b) { a = ((a % MOD) - (b % MOD) + MOD) % MOD; }
+void modmul(int &a, int b) { a = ((a % MOD) * (b % MOD)) % MOD; }
+// ================================== take ip/op like vector,pairs directly!==================================
+template <typename typC, typename typD>
+istream &operator>>(istream &cin, pair<typC, typD> &a) { return cin >> a.first >> a.second; }
+template <typename typC>
+istream &operator>>(istream &cin, vector<typC> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+    return cin;
+}
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const pair<typC, typD> &a) { return cout << a.first << ' ' << a.second; }
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const vector<pair<typC, typD>> &a)
+{
+    for (auto &x : a)
+        cout << x << '\n';
+    return cout;
+}
+template <typename typC>
+ostream &operator<<(ostream &cout, const vector<typC> &a)
+{
+    int n = a.size();
+    if (!n)
+        return cout;
+    cout << a[0];
+    for (int i = 1; i < n; i++)
+        cout << ' ' << a[i];
+    return cout;
+}
+// ===================================END Of the input module ==========================================
+
+void solve()
+{
+    int n, c;
+    cin >> n >> c;
+
+    vector<pair<int, int>> v(n);
+    int val;
+    int mini = INT_MAX;
+    int index = -1;
+    fr(i, n)
+    {
+        cin >> val;
+        if (val + (i + 1) < mini)
+        {
+            mini = val + (i + 1);
+            index = i;
+        }
+        v[i].first = min(val + (i + 1), val + (n - i));
+        v[i].second = i;
+    }
+
+    srt(v);
+
+    if (mini > c)
+    {
+        cout << 0 << endl;
+        return;
+    }
+    int count = 1;
+    int i = 0;
+    int sum = mini;
+
+    while (i < n)
+    {
+        if (v[i].second == index)
+        {
+            i++;
+            continue;
+        }
+        sum += v[i].first;
+        if (sum > c)
+            break;
+
+        count++;
+        i++;
+    }
+
+    cout << count << endl;
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
